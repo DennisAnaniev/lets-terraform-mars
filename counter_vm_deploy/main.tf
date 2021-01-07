5b0be2dec55458030e28103e7e328e785263db08
Denis Ananev <denis.ananev@relexsolutions.com>
2021-01-07 14:28:01 +0200

    Test CI

--- counter_vm_deploy/main.tf
+++ counter_vm_deploy/main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "eu-central-1"
 }
 resource "aws_instance" "my_ubuntu" {
-  ami           = "ami-0489277f0e9a94f8d"
-  instance_type = "t3.micro"
+  ami           = "ami-0489277f0e9a94f8d44"
+  instance_type = "t3.medium"
   count = 3
   tags = {
     "Name"   = "Web_server"
