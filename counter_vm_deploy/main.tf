7a51644bb9eb4caac818a6480f9e0cbfa16f3f9d
Denis Ananev <denis.ananev@relexsolutions.com>
2021-01-07 15:15:29 +0200

    Test_workflow

--- counter_vm_deploy/main.tf
+++ counter_vm_deploy/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "my_ubuntu" {
   ami           = "ami-0489277f0e9a94f8d44"
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
   count = 3
   tags = {
     "Name"   = "Web_server"
